04d963d548dada44fc39d89508c33921587bb65d
andrea <andreablass11@gmail.com>
2021-09-28 20:37:46 -0500

    update

--- app/Http/Controllers/MascotaController.php
+++ app/Http/Controllers/MascotaController.php
@@ -12,8 +12,38 @@ class MascotaController extends Controller
     function test(Request $request){
         return response()->json(["error"=>false,"message"=>"Consulta Exitosa"],200);
     }
-
     function crearMascota(Request $request){
+        DB::beginTransaction();
+        $objetoMascota = new Mascota();
+        try{
+            if(!empty($request->id)){
+                $objetoMascota=Mascota::find($request->id);
+                $objetoMascota->raza = $request->raza;
+                $objetoMascota->tamanio = $request->tamanio;
+                $objetoMascota->save();
+                DB::commit();
+                return response()->json([
+                    "error"=>false,
+                    "message"=>"Registro actualizado",
+                    "Detalles"=>$objetoMascota]
+                    , 200);
+            }else{
+                $objetoMascota->raza = $request->raza;
+                $objetoMascota->tamanio = $request->tamanio;
+                $objetoMascota->save();
+                DB::commit();
+                return response()->json([
+                    "error"=>false,
+                    "message"=>"Registro Creado",
+                    "Detalles"=>$objetoMascota]
+                    , 201);
+            }
+        }catch(QueryException $queryException){
+        DB::rollback();
+        return response()->json($queryException->errorInfo,500);
+        }
+    }
+       /*function crearMascota(Request $request){
         DB::beginTransaction();
         try{
         $objetoMascota = new Mascota();
@@ -27,6 +57,6 @@ class MascotaController extends Controller
         return response()->json($queryException->errorInfo,500);
         }
     
-    }
+    }*/
 
 }
